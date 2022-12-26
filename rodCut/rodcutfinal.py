891596323092661b5379067d3e0a09c5b9952b82
常旗 <changjiangmingxj@163.com>
2022-12-26 20:09:35 +0800

    rod cut all possible solutions

--- rodCut/rodcutfinal.py
+++ rodCut/rodcutfinal.py
@@ -1,4 +1,4 @@
-def maxCut(n,m) -> int:
+def maxCut(n, m) -> int:
     if n == 0:
         return 0
     if n == 1:
@@ -6,16 +6,17 @@ def maxCut(n,m) -> int:
 
     Max = 0
     for i in range(1, n + 1):
-        if n-i > len(m) and m[n-i] >= 0:
-            subMax = m[n-i]
+        if n - i > len(m) and m[n - i] >= 0:
+            subMax = m[n - i]
         else:
             subMax = maxCut(n - i)
         if p[i] + subMax > Max:
-           Max = p[i] + subMax
+            Max = p[i] + subMax
 
-    m.insert(n,Max)
+    m.insert(n, Max)
     return Max
 
-p = [0,1,5,8,9,10,17,17,20,24,30]
-m = [0,1]
-print(maxCut(1000,m))
+
+p = [0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30]
+m = [0, 1]
+print(maxCut(1000, m))
